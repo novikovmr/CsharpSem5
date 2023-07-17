8877cf9db035034271335ae9e5607ba2047115ae
Max <maksim101101@gmail.com>
2023-07-17 20:17:08 +0400

    Выполнил задание 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Clear();
+
+void inputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+int releaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 != 0)
+            sum += array[i];
+    return sum;
+}
+
+Console.Write("Введите количество элементов массива: ");
+int numberOfElements = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[numberOfElements];
+inputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма нечетных элементов массива: {releaseArray(array)}");
